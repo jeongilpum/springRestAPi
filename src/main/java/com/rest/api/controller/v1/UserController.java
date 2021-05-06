539b18090b8743e64610da9283d96b2751af3ece
jeong <jeilpm@naver.com>
2021-05-06 16:00:46 +0900

    Initial commit

---
+++ src/main/java/com/rest/api/controller/v1/UserController.java
@@ -0,0 +1,73 @@
+package com.rest.api.controller.v1;
+
+import com.rest.api.advice.exception.CUserNotFoundException;
+import com.rest.api.entity.User;
+import com.rest.api.model.response.CommonResult;
+import com.rest.api.model.response.ListResult;
+import com.rest.api.model.response.SingleResult;
+import com.rest.api.repo.UserJpaRepo;
+import com.rest.api.service.ResponseService;
+import io.swagger.annotations.*;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.*;
+
+@Api(tags = {"2. User"})
+@RequiredArgsConstructor
+@RestController
+@RequestMapping(value="/v1")
+public class UserController {
+
+    private final UserJpaRepo userJpaRepo;
+    private  final ResponseService responseService;
+
+
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "X-AUTH-TOKEN", value = "로그인 성공 후 access_token", required = true, dataType = "String", paramType = "header")
+    })
+    @ApiOperation(value = "회원 리스트 조회", notes = "모든 회원을 조회한다")
+    @GetMapping(value = "/users")
+    public ListResult<User> findAllUser() {
+        return responseService.getListResult(userJpaRepo.findAll());
+    }
+
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "X-AUTH-TOKEN", value = "로그인 성공 후 access_token", required = false, dataType = "String", paramType = "header")
+    })
+    @ApiOperation(value = "회원 단건 조회", notes = "userId로 회원을 조회한다.")
+    @GetMapping(value = "/user")
+    public SingleResult<User> findUserById(@ApiParam(value= "언어", defaultValue = "ko") @RequestParam String lang)  {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String id = authentication.getName();
+        return responseService.getSingleResult(userJpaRepo.findByUid(id).orElseThrow(CUserNotFoundException::new));
+    }
+
+
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "X-AUTH-TOKEN", value = "로그인 성공 후 access_token", required = false, dataType = "String", paramType = "header")
+    })
+    @ApiOperation(value = "회원 수정", notes = "회원정보를 수정한다")
+    @PutMapping(value = "/user")
+    public SingleResult<User> modify(
+            @ApiParam(value = "회원번호", required = true) @RequestParam long msrl,
+            @ApiParam(value = "회원이름", required = true) @RequestParam String name) {
+        User user = User.builder()
+                .msrl(msrl)
+                .name(name)
+                .build();
+        return responseService.getSingleResult(userJpaRepo.save(user));
+    }
+
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "X-AUTH-TOKEN", value = "로그인 성공 후 access_token", required = false, dataType = "String", paramType = "header")
+    })
+    @ApiOperation(value = "회원 삭제", notes = "userId로 회원정보를 삭제한다")
+    @DeleteMapping(value = "/user/{msrl}")
+    public CommonResult delete(
+            @ApiParam(value = "회원번호", required = true) @PathVariable long msrl) {
+        userJpaRepo.deleteById(msrl);
+        return responseService.getSuccessResult();
+    }
+
+}
